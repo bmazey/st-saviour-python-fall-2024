0e2a7245acc87c1823a4dd53864ce9c167288bda
xinbei <xinbei0706@gmail.com>
2024-09-25 10:32:35 -0400

    test passing

--- interview/password.py
+++ interview/password.py
@@ -16,11 +16,30 @@ def generate_password() -> str:
 
     # TODO implement generate_password function
     import random 
-    alphabet = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z '
-    r = alphabet  [ random.randit  (a,len(alphabet) - 1) ]
-    password = alphabet [r]
-    print ('random alphabet r '+ str(r))
-    print ('random password'+1 )
+
+    password = ''
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    r = alphabet[random.randint(0, len(alphabet) - 1)]
+    password += r
+    password += r
+    password += r
+    password += r
+    password += r
+    
+    digit = '0123456789'
+    r = digit[random.randint(0, len(digit)-1)]
+    password += r
+    password += r
+    password += r
+    password += r
+
+    symbol = '!@#$%^&*'
+    r = symbol[random.randint(0, len(symbol)-1)]
+    password += r
+
+    return password
+    
     
     
  
