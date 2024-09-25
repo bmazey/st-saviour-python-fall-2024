7b92fe28c63c7acbf96b2c8e6a9c34199a7603b1
megan <mmccabe@stsavoiur.org>
2024-09-25 11:28:58 -0400

    first commit

--- interview/rounder.py
+++ interview/rounder.py
@@ -9,3 +9,15 @@ def round(number: float) -> int:
     # TODO implement round function
 
     return 0
+
+    def round(number: 0 == float) -> int: 0
+
+    def round(number: .5 == float) -> int: 1
+
+    def round(number: 1.0 == float) -> int: 1
+
+    def round(number: 1.2 == float) -> int: 1
+
+    def round(number: 7.6 == float) -> int: 8
+
+    def round(number: 7.9 == float) -> int: 8
