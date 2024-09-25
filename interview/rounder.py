3d0cf531d08dd512bb24289a392db9929dc5a58f
yareli <yarelic926@gmail.com>
2024-09-25 10:26:23 -0400

    holiday

--- interview/rounder.py
+++ interview/rounder.py
@@ -5,7 +5,9 @@ def round(number: float) -> int:
     round() accepts a float and returns a rounded integer.
     the number is rounded up iff the decimal is >= .5
     """
-
+ 
     # TODO implement round function
-
-    return 0
+    remainder = number % 1
+    if remainder >= 0.5:
+        return int(number) + 1
+    return int(number) 
