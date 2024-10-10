306d884e28b4b151ea29c8e211845c3afb47a245
xinbei <xinbei0706@gmail.com>
2024-10-10 11:52:03 -0400

    xinbei interview

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
+number = 0
+while number <= 100:
+    print (number)
+    number +=5
+
+for number in range (0,101):
+    if number %5 == 0:
+        print (number)
+
+for number in range (10):
+    for n in range(10):
+       print(number)
+    print ('interation' + str(1) + 'done')
+
+# add something at the end of the list is append; use int and loop 
+# when you remove an element, everything at the right hand side should be shifted 1 position
+# pop could remove the elemnt too
+# number does not equal (!=), result. append(number) and print it out  
+
+
 
