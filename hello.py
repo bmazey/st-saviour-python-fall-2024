20e9647fad31f2efb47ac92b3d09fda77276f541
brandon mazey <brandon@brandons-Air.localdomain>
2024-10-11 11:54:37 -0400

    notes

--- hello.py
+++ hello.py
@@ -1,15 +1,60 @@
-import random
 
 if __name__ == '__main__':
     # this file is provided for experimentation purposes
     # print('new dawn, new day')
 
-    h = 'abcdefghijklmnopqrstuvwxyz'
+    # introduction to lists
+    # numbers = [0, 1, 2, 2, 2, 3]
+    # numbers.pop(0)
+    # print(numbers)
+    # print(numbers[5])
+    # print(numbers)
+    # numbers[0] = 22
+    # print(numbers)
 
-    r = random.randint(0, len(h) - 1)
-    l = h[r]
+    # numbers.remove(2)
+    # print(numbers)
+    # numbers[6] = 6
+    # print(numbers)
+    # for number in numbers:
+    #     print(number)
 
-    s = 'xoxoxoxoxoxoxoxo'
-    print(s[::])
-    print(s[::2])
-    print(s[1::2])
+    # out of bounds error!
+    # numbers[6] = 6
+    # print(numbers[6])
+
+    # numbers.append(7)
+    # print(numbers[6])
+
+    # numbers.remove(2)
+    # print(numbers)
+
+    # TODO remove all instances of the number 2
+    # numbers = [0, 2, 2, 2, 3, 1]
+    # print(numbers)
+    # result = []
+    # for number in numbers:
+    #     if number != 2:
+    #         result.append(number)
+    # print(result)
+
+
+    # numbers.insert(0, 15)
+    # print(numbers)
+
+    # mixed types are bad
+    # things = ['we', 'started', 'out', 'friends', 7]
+    # for thing in things:
+    #     print('here\'s the thing: ' + thing)
+
+    # tuples
+    # name, date of birth, height, valid?
+    id_card = ('Brandon J. Mazey', '05/26', 188, True)
+    print(id_card[0])
+
+    # tuples are immutable
+    # id_card[0] = 'Capt. Brandon J. Mazey'
+
+    # for element in id_card:
+    #     print(str(element))
+    
