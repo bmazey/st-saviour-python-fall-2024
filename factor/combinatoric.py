e36b5e71e3e7f8238acd9b531500ad714413b2ab
flowerschaos <cfactionbackup@gmail.com>
2024-10-24 11:29:59 -0400

    terrible wretched combining

--- factor/combinatoric.py
+++ factor/combinatoric.py
@@ -2,8 +2,13 @@
 
 def factorial(n: int) -> int:
     # TODO implement for +10 bonus
-    return 0
+    index = 1
+    product = 1
+    while index <= n:
+        product *= index
+        index += 1
+    return product
 
 def choose(n: int, k: int) -> int:
     # TODO implement for +10 bonus
-    return 0
+    return factorial(n)/(factorial(k)*factorial(n-k))
