31cfa0b1b413d438a1034dd03c57b5cbec9cb36c
megan <mmccabe@stsavoiur.org>
2024-10-30 10:48:41 -0400

    lab 2

--- factor/combinatoric.py
+++ factor/combinatoric.py
@@ -2,7 +2,12 @@
 
 def factorial(n: int) -> int:
     # TODO implement for +10 bonus
-    return 0
+    result = 1 
+    i = 1 
+    while i <= n: 
+        result *= i 
+        i += 1 
+    return result 
 
 def choose(n: int, k: int) -> int:
     # TODO implement for +10 bonus
